dff1b8644c0a2fc104ec19014ece0ca1a827006e
Marc <3913407+MarcGM@users.noreply.github.com>
2024-06-26 01:25:29 +0200

    TaskCreationController method save, done

--- TaskManager/src/main/java/com/fintonic/taskmanager/controllers/TaskCreationController.java
+++ TaskManager/src/main/java/com/fintonic/taskmanager/controllers/TaskCreationController.java
@@ -4,10 +4,10 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.validation.annotation.Validated;
 
 import com.fintonic.taskmanager.models.dtos.TaskDTO;
 import com.fintonic.taskmanager.services.TaskCreationService;
@@ -18,18 +18,17 @@ public class TaskCreationController {
 	
 	private final TaskCreationService service;
 
-	public TaskCreationController(TaskCreationService service, TaskCreationService taskCreationService) {
+	public TaskCreationController(TaskCreationService taskCreationService) {
 		this.service = taskCreationService;
-		
 	}
 	
 	@PostMapping(path = "/createTask")
-	public ResponseEntity<TaskDTO> createTask(TaskDTO taskDto) {
+	public ResponseEntity<TaskDTO> createTask(@RequestBody TaskDTO taskDto) {
 		Optional<TaskDTO> returnedTask = service.saveTask(taskDto);
 		
 		if(returnedTask.isPresent())
 			
-			return new ResponseEntity<>(HttpStatus.CREATED);
+			return new ResponseEntity<>(returnedTask.get(),HttpStatus.CREATED);
 		else
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
